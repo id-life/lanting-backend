1b86e24f5d0fa151c0e5213fc438529153c55c97
Lo <l709937065@gmail.com>
2025-09-16 09:30:37 +0800

    feat(database): create users, email_whitelists, and pending_archive_origs tables with foreign key constraints

--- prisma/migrations/20250916012945_auth_and_pending_archives/migration.sql
+++ prisma/migrations/20250916012945_auth_and_pending_archives/migration.sql
@@ -1,4 +1,24 @@
 -- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `githubId` VARCHAR(191) NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `avatar` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+    `githubSecret` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_githubId_key`(`githubId`),
+    UNIQUE INDEX `users_username_key`(`username`),
+    INDEX `users_githubId_idx`(`githubId`),
+    INDEX `users_username_idx`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `email_whitelists` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` VARCHAR(191) NOT NULL,
@@ -34,7 +54,7 @@ CREATE TABLE `pending_archive_origs` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `email_whitelists` ADD CONSTRAINT `email_whitelists_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `email_whitelists` ADD CONSTRAINT `email_whitelists_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `pending_archive_origs` ADD CONSTRAINT `pending_archive_origs_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `pending_archive_origs` ADD CONSTRAINT `pending_archive_origs_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
